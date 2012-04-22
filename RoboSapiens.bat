39a6e2f876e21ec5787f6ea5597de063a7e5b217
bugos <baggos12@gmail.com>
2012-04-22 14:43:04 +0300

    changed the handling of additional options

--- RoboSapiens.bat
+++ RoboSapiens.bat
@@ -1,8 +1,11 @@
 ::|| RoboSapiens V3.0.0
 ::|| Written by bugos//18-4-2012
 ::|| Let's you play for free in the local netcafe
-::|| %1: set the time to hh:mm
-::|| %2: if set to k, just kill the bar
+::|| %time%: the timer of the guards is set to this values.
+::|| 		 if set to k, it kills the guards and exits.
+::||         if set to s, it opens a system command promt.
+::||taken from %1 (arg1) or from cin
+
 ::|| 010.exe is process.exe from http://retired.beyondlogic.org/solutions/processutil/processutil.htm
 ::|| -k to kill, -s to suspend, -r to resume
 ::|| 020.exe is PsExec from http://technet.microsoft.com/en-us/sysinternals/bb897553
@@ -13,7 +16,7 @@ reg.exe ADD "HKCU\Software\Sysinternals\PsExec" /v EulaAccepted /t REG_DWORD /d 
 :start
 :://Take input from args if they exist, else from cin
 IF "%1"=="" (
-	echo hey man! gimme numbers
+	echo hey man! gimme something
 	set /p time=
 ) ELSE (
 	set time=%1
@@ -21,14 +24,16 @@ IF "%1"=="" (
 
 :://Additional options
 IF "%time%"=="s" (start 020 cmd && goto start)
+IF "%time%"=="k" (set kill_only=1 %% set time=00:05)
 
-:://Input checking ::(Minor bug with multiple errors, displays last one only)
+:://Input checking 
+    ::(Minor bug with multiple errors, displays last one only)
 set "error="
 IF not "%time:~2,1%"==":" (set error=colon missing)
 IF not "%time:~5,1%"==""  (set error=more than 5 chars)
 IF     "%time:~4,1%"==""  (set error=less than 5 chars) 
-IF not "%time:~0,1%"=="0" (set error=hours1 not 0)
-::TODO: if numbers checking
+IF not "%time:~0,1%"=="0" (set error=more than 9 hours)
+    ::TODO: if numbers checking
 IF defined error (
 echo Error! : %error%. Try hh:mm
 goto start
@@ -48,7 +53,7 @@ echo 010 -s svhost2.exe>>1.bat
 echo net stop svchost3>>1.bat
 echo 010 -k WEBMAIN1.exe>>1.bat
 
-IF /i "%2"=="k" (echo I will just kill then) && goto launch_rocket
+IF /i "%kill_only%"=="1" (echo I will just kill then) && goto launch_rocket
 
 :://Edit the time
 echo d:>>1.bat
